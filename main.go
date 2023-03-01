2fcaab73c8cefe0ff3eb33cf11f0c5d26a238646
Michael Marigliano <michael.marigliano@ingka.com>
2023-03-01 17:19:46 +0100

    coffee time

--- main.go
+++ main.go
@@ -13,25 +13,7 @@ const (
 	beansPerCup = 15
 )
 
-func testies() {
-	var number int
-	fmt.Scanf("%d", &number)
-	switch true {
-	case number < 0:
-		fmt.Print("Negative!")
-	case number == 0:
-		fmt.Print("Zero!")
-	case number > 0:
-		fmt.Print("Positive!")
-	}
-}
-
 func coffee() {
-	gatherInfo()
-	printCoffeeProccess()
-}
-
-func gatherInfo() {
 	var (
 		water int
 		milk  int
@@ -46,18 +28,40 @@ func gatherInfo() {
 	fmt.Scan(&beans)
 	fmt.Println("Write how many cups of coffee you will need: ")
 	fmt.Scan(&cups)
-	calcNeeded(cups)
-	calculateMinCups(water, milk, beans, cups)
+	calculateNeededIngredients(cups)
+	minCups := calculateMinCups(water, milk, beans)
+	switch true {
+	case cups == minCups:
+		fmt.Println("Yes, I can make that amount of coffee")
+		printCoffeeProcess()
+	case cups >= minCups:
+		fmt.Printf("No, I can only make %d cups of coffee.\n", minCups)
+	case cups < minCups:
+		fmt.Printf("Yes, I can make that amount of coffee (and even %d more than that) \n", minCups-cups)
+		printCoffeeProcess()
+	}
 }
 
-func calcNeeded(cups int) {
+func calculateNeededIngredients(cups int) {
 	fmt.Printf("For %d cups of coffee you will need: \n", cups)
 	fmt.Printf("%d ml of water \n", cups*waterPerCup)
 	fmt.Printf("%d ml of milk \n", cups*milkPerCup)
 	fmt.Printf("%d g of coffee \n", cups*beansPerCup)
 }
 
-func printCoffeeProccess() {
+func calculateMinCups(water, milk, coffeeBeans int) int {
+	minCups := water / waterPerCup
+	if milk/milkPerCup < minCups {
+		minCups = milk / milkPerCup
+	}
+	if coffeeBeans/beansPerCup < minCups {
+		minCups = coffeeBeans / beansPerCup
+	}
+
+	return minCups
+}
+
+func printCoffeeProcess() {
 	fmt.Println("Starting to make a coffee")
 	fmt.Println("Grinding coffee beans")
 	fmt.Println("Boiling water")
@@ -67,32 +71,5 @@ func printCoffeeProccess() {
 	fmt.Println("Coffee is ready!")
 }
 
-func calculateMinCups(water int, milk int, beans int, cups int) {
-	var (
-		minCupsWater = (water - waterPerCup*cups) / waterPerCup
-		minCupsMilk  = (milk - milkPerCup*cups) / milkPerCup
-		minCupsBeans = (beans - beansPerCup*cups) / beansPerCup
-		minCups      int
-	)
-
-	// Check the lowest number
-	if minCupsWater <= minCupsMilk && minCupsWater <= minCupsBeans {
-		minCups = minCupsWater
-	} else if minCupsMilk <= minCupsWater && minCupsMilk <= minCupsBeans {
-		minCups = minCupsMilk
-	} else {
-		minCups = minCupsBeans
-	}
-
-	switch true {
-	case minCups == cups:
-		fmt.Println("Yes, I can make that amount of coffee")
-	case minCups < 0 && cups+minCups > 0:
-		fmt.Printf("No, I can make only %d cups \n", minCups+cups)
-	case minCups > cups:
-		fmt.Printf("Yes, I can make that amount of coffe (and even %d more than that)", minCups-cups)
-	case minCups < 0 && cups+minCups < 0:
-		fmt.Println("No, I can only make 0 cups of coffee")
-	}
-
+func testies() {
 }
